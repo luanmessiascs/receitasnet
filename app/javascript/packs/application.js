3142a05fadeaaebfa3d0d12ea103c11c9e3c3a2d
Luan Messias <luan44058@gmail.com>
2021-11-19 21:52:28 -0300

    Melhorando o layout da aplicação

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../../assets/images/receitanet.png"
+import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js"
 
 Rails.start()
 Turbolinks.start()
